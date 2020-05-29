b21bc4c7d74ffdbcced1439f56acd77bb4de3c6a
Arpan <arpanchowdhury050@gmail.com>
2020-05-29 13:10:11 +0530

    Updates

---
+++ precache-manifest.578d13327c4ef24c87e6d0812f4228bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b461b28455d3d1346c5dddae13a4fc06",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "2674dbdc20147526e533",
+    "url": "/Recipe-App/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "b6162d03628024f86826",
+    "url": "/Recipe-App/static/css/main.2859cefe.chunk.css"
+  },
+  {
+    "revision": "2674dbdc20147526e533",
+    "url": "/Recipe-App/static/js/2.f8c87b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.f8c87b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6162d03628024f86826",
+    "url": "/Recipe-App/static/js/main.08926be8.chunk.js"
+  },
+  {
+    "revision": "a19e7221d9283f34c76a",
+    "url": "/Recipe-App/static/js/runtime-main.8047b5ae.js"
+  }
+]);
